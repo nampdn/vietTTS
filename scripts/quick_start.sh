d3131f77cb2becf167ba3a75cf4e8943356c34dc
Nam Pham <nampdn@protonmail.com>
2022-04-29 18:10:41 +0700

    Update quick_start.sh

--- scripts/quick_start.sh
+++ scripts/quick_start.sh
@@ -1,11 +1,12 @@
-if [ ! -f assets/infore/hifigan/g_00800000 ]; then
+declare HIFIGAN_CHECKPOINT=assets/infore/hifigan/g_00800000
+if [ ! -f $HIFIGAN_CHECKPOINT ]; then
   pip3 install gdown
   echo "Downloading models..."
   mkdir -p -p assets/infore/{nat,hifigan}
-  gdown --id 16UhN8QBxG1YYwUh8smdEeVnKo9qZhvZj -O assets/infore/nat/duration_latest_ckpt.pickle
-  gdown --id 1-8Ig65S3irNHSzcskT37SLgeyuUhjKdj -O assets/infore/nat/acoustic_latest_ckpt.pickle
-  gdown --id 10SFOlAduG20TdjGC5e1Jod_vJIpxkD6u -O assets/infore/hifigan/g_00800000
-  python -m vietTTS.hifigan.convert_torch_model_to_haiku --config-file=assets/hifigan/config.json --checkpoint-file=assets/infore/hifigan/g_00800000
+  gdown 1-3OgKYHf77n0pxX0GDsfNlL9yLAwx61l -O assets/infore/nat/duration_latest_ckpt.pickle
+  gdown 1-S6_fsLCc0JqROJgjfHa4cxC5BivvJEy -O assets/infore/nat/acoustic_latest_ckpt.pickle
+  gdown 1ZrzXlBa2ugGGaPcE8DZA98hTBSiBNmJu -O $HIFIGAN_CHECKPOINT
+  python -m vietTTS.hifigan.convert_torch_model_to_haiku --config-file=assets/hifigan/config.json --checkpoint-file=$HIFIGAN_CHECKPOINT
 fi
 
 echo "Generate audio clip"
