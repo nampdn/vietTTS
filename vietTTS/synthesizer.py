b73b1e46341f9faefc6498c6f0077abc2db4e013
Nam Pham <nampdn@protonmail.com>
2022-06-01 01:01:34 +0700

    Update synthesizer.py

--- vietTTS/synthesizer.py
+++ vietTTS/synthesizer.py
@@ -2,7 +2,8 @@ import re
 import unicodedata
 from argparse import ArgumentParser
 from pathlib import Path
-
+import matplotlib.pyplot as plt
+import jax
 import soundfile as sf
 
 from .hifigan.mel2wave import mel2wave
@@ -12,6 +13,7 @@ from .nat.text2mel import text2mel
 parser = ArgumentParser()
 parser.add_argument("--text", type=str)
 parser.add_argument("--output", default="clip.wav", type=Path)
+parser.add_argument("--outputmel", default="clip", type=Path)
 parser.add_argument("--sample-rate", default=16000, type=int)
 parser.add_argument("--silence-duration", default=-1, type=float)
 parser.add_argument("--lexicon-file", default=None)
@@ -34,6 +36,15 @@ def nat_normalize_text(text):
 text = nat_normalize_text(args.text)
 print("Normalized text input:", text)
 mel = text2mel(text, args.lexicon_file, args.silence_duration)
+
+# Output mel
+plt.figure(figsize=(10, 5))
+plt.imshow(mel[0].T, origin="lower", aspect="auto")
+plt.savefig(str(args.outputmel))
+plt.close()
+mel = jax.device_get(mel)
+mel.tofile("clip.png")
+
 wave = mel2wave(mel)
 print("writing output to file", args.output)
 sf.write(str(args.output), wave, samplerate=args.sample_rate)
