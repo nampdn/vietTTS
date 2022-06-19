c32a1a90dcfd5644738c768738f60d64ff55562b
Restyled.io <commits@restyled.io>
2022-06-19 13:52:45 +0000

    Restyled by black

--- vietTTS/nat/zero_silence_segments.py
+++ vietTTS/nat/zero_silence_segments.py
@@ -15,17 +15,17 @@ args = parser.parse_args()
 
 files = sorted(FLAGS.data_dir.glob("*.TextGrid"))
 for fn in tqdm(files):
-  tg = TextGrid.fromFile(str(fn.resolve()))
-  wav_fn = FLAGS.data_dir / f"{fn.stem}.wav"
-  sr, y = wavfile.read(wav_fn)
-  y = np.copy(y)
-  for phone in tg[1]:
-    mark = phone.mark.strip().lower()
-    if len(mark) == 0:
-      mark = "sil"
-    if mark in FLAGS.special_phonemes:
-      l = int(phone.minTime * sr)
-      r = int(phone.maxTime * sr)
-      y[l:r] = 0
-  out_file = args.output_dir / f"{fn.stem}.wav"
-  wavfile.write(out_file, sr, y)
+    tg = TextGrid.fromFile(str(fn.resolve()))
+    wav_fn = FLAGS.data_dir / f"{fn.stem}.wav"
+    sr, y = wavfile.read(wav_fn)
+    y = np.copy(y)
+    for phone in tg[1]:
+        mark = phone.mark.strip().lower()
+        if len(mark) == 0:
+            mark = "sil"
+        if mark in FLAGS.special_phonemes:
+            l = int(phone.minTime * sr)
+            r = int(phone.maxTime * sr)
+            y[l:r] = 0
+    out_file = args.output_dir / f"{fn.stem}.wav"
+    wavfile.write(out_file, sr, y)
